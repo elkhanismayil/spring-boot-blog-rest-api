0ceeeae0a825db09544d7c38cc767b06f4b97cb1
Elxan <elxan310@gmail.com>
2022-08-10 11:37:01 +0400

    - added Swagger UI dependency - changed pom.xml file - changed SecurityConfig class - added SwaggerConfig class

---
+++ src/main/java/com/springboot/blog/config/SwaggerConfig.java
@@ -0,0 +1,61 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.PathProvider;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Blog REST APIs",
+                "Spring Boot Blog REST API Documentation",
+                "1.0",
+                "Terms of service",
+                new Contact("Elkhan", "www.example.com", "elxan310@gmail.com"),
+                "License of API",
+                "API License URL",
+                Collections.emptyList()
+        );
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = scope;
+        return List.of(new SecurityReference("JWT", scopes));
+    }
+}
